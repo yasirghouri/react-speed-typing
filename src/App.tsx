2d91f773ff85567e9c9d970588550243052440aa
yasirghouri <yasir.ag7@gmail.com>
2023-02-01 23:07:55 +0500

    setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-4xl text-center text-blue-200">
       React Typescript
     </div>
   );
